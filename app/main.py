460aeeccf9518a66040f54b0f09a40f50b646a49
ABDUKHAMID MIRZAAKHMED <133525851+ABDULHAM1D0@users.noreply.github.com>
2026-03-03 18:38:22 +0300

    Add comment to bgr_to_rgb function
    Added a comment to clarify the purpose of the bgr_to_rgb function.

--- app/main.py
+++ app/main.py
@@ -8,4 +8,6 @@ def save_uploaded_image(uploaded_file, save_path="temp.jpg"):
     return save_path
 
 def bgr_to_rgb(image):
+    # convert image brg to rgb color.
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
